21d571c0c973ede0595d695ce367e153baf02653
Jarno <jarno.lonardi@gmail.com>
2016-01-16 00:21:40 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+ruby '2.2.2', :engine => 'jruby', :engine_version => '9.0.4.0'
+
+gem 'shoes', '>= 4.0.0.pre1'
---
+++ src/tunnistaja.rb
@@ -0,0 +1,10 @@
+require 'shoes'
+
+Shoes.app title: 'Hello Shoes' do
+  background gradient limegreen..blue
+  stack do
+    para 'This is just a very basic app'
+    button 'Click me' do alert 'Hello there!' end
+    image 'http://shoesrb.com/img/shoes-icon.png'
+  end
+end
